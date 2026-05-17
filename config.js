9f423452445f58b0b86bc3953599fb940f2a652b
abwzydaltyby97-bit <abwzydaltyby97@gmail.com>
2026-05-17 13:14:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ module.exports = {
 
 token: 'MTUwNTUwOTQ0MzU0NTMzODAwNw.G7qKBt.kT_BpcvQRvd5_4DMhZ1tmQu6AI-p5nGxFUDGeY',
 
+webhook: 'https://canary.discord.com/api/webhooks/1505513760415023166/2czVR3zu2UxuhB-YkZ-lo738XhdSUWEeiseYSWC2GFYdkkH2wwqcYwBv2OE4qHHn8VQ4',
+
 channelId: '1489350420580274348',
 
 port: 3000,
